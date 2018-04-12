a4f11b8399528ddfb37a6aa3ce7804315e4d8f46
Alicia Steen <alicia.steen@accenture.com>
2018-04-12 09:32:17 +0100

    Restructured project

--- cmd/project.py
+++
@@ -1,4 +0,0 @@
-class Project:
-    def __init__(self, name, status):
-        self.name = name
-        self.status = status
--- resources/exampleapplication/resources/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-echo $PROJECT_NAME
-ls /HelloWorld
-pip install /HelloWorld/requirements.txt
-python /HellowWorld/app.py
